14a866a79f889b2da9adcb0c831941d23797df0d
Adriano Carvalho <adrianocarvalhowebmail@gmail.com>
2020-10-17 17:15:10 -0300

    controller de Pessoa

--- cmbse/app/controllers/pessoas_controller.rb
+++ cmbse/app/controllers/pessoas_controller.rb
@@ -1,13 +1,62 @@
 class PessoasController < ApplicationController
-  def index
-  end
+  before_action :set_pessoa, only: [:show, :edit, :update, :destroy]
 
-  def edit
+  def index
+    @pessoas = Pessoa.all
   end
 
   def new
+    @pessoa = Pessoa.new
+  end
+
+  def create
+    @pessoa = Pessoa.new(pessoa_params)
+
+    respond_to do |format|
+      if @pessoa.save
+        format.html { redirect_to @pessoa, notice: 'Pessoa criada com sucesso.' }
+        format.json { render :show, status: :created, location: @pessoa }
+      else
+        format.html { render :new }
+        format.json { render json: @pessoa.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @pessoa.update(pessoa_params)
+        format.html { redirect_to @pessoa, notice: 'Pessoa atualizada com sucesso.' }
+        format.json { render :show, status: :ok, location: @pessoa }
+      else
+        format.html { render :edit }
+        format.json { render json: @pessoa.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def edit
   end
 
   def show
   end
+
+  def destroy
+    @pessoa.destroy
+    respond_to do |format|
+      format.html { redirect_to pessoas_url, notice: 'Pessoa deletada com sucesso.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_pessoa
+    @pessoa = Pessoa.find(params[:id])
+  end
+
+  def pessoa_params
+    params.require(:pessoa).permit(:nome, :sobrenome)
+  end
+
 end
