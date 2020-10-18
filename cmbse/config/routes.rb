21ccb91d06433b99d894737f72c9ca995fa012fc
Adriano Carvalho <adrianocarvalhowebmail@gmail.com>
2020-10-18 20:56:58 -0300

    Rotas para cadastro de contato

--- cmbse/config/routes.rb
+++ cmbse/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'inicio#index'
-
+  
   resources :pessoas
   resources :tipo_contatos
+  resources :contato_pessoas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
